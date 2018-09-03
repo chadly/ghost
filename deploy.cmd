f7c3ce9ccb52a29be256b8e67f2526172a1ffb66
Chad Lee <chad.lee@civicsource.com>
2018-09-03 08:09:04 -0500

    Tweak yarning in build script some more
    https://github.com/stefangordon/kudu-yarn/issues/1#issuecomment-388394681

--- deploy.cmd
+++ deploy.cmd
@@ -99,9 +99,10 @@ call :SelectNodeVersion
 
 :: 3. Install Yarn
 echo Verifying Yarn Install.
-call :ExecuteCmd !NPM_CMD! config set prefix "D:\home\tools"
 call :ExecuteCmd !NPM_CMD! install yarn -g
-set path=%path%;D:\home\tools
+FOR /F "tokens=* USEBACKQ" %%F IN (`npm config get prefix`) DO (
+SET "PATH=%PATH%;%%F"
+)
 
 :: 4. Install Yarn packages
 echo Installing Yarn Packages.
